95c24271769133b912b59ad6936e2951595baf20
Abhishek6416 <abhishekkr6416@gmail.com>
2023-01-04 16:13:07 +0530

    PaymentPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Payment from "./PaymentPage/Payment";
 
 function App() {
   return (
     <div className="App">
       <h1>MyFitnesspal </h1>
+      <Payment/>
     </div>
   );
 }
---
+++ src/PaymentPage/Payment.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Payment() {
+  return (
+    <div>
+
+        <h1>PaymentPage</h1>
+    </div>
+  )
+}
