998da577bf807da65cc96627a933d0d628bf03d9
Abhishek6416 <abhishekkr6416@gmail.com>
2023-01-08 07:17:03 +0530

    OptionsGrid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TopSection from "./Components/premium/TopSection";
+import OptionsGrid from "./Components/premium/Optionsgrid";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>MyFitnesspal </h1>
       <TopSection/>
+      <OptionsGrid/>
      
     </div>
   );
