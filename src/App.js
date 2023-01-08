84b1735201fcb71332ac6a919bd4c08b967f0d93
Abhishek6416 <abhishekkr6416@gmail.com>
2023-01-08 09:43:22 +0530

    AddedRoutes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import TopSection from "./Components/premium/TopSection";
-import OptionsGrid from "./Components/premium/Optionsgrid";
-import YourGoalsSection from "./Components/premium/YourGoalsSection";
-import VictoryStories from "./Components/premium/VictoryStories";
-import FrequentlyAskedQuestions from "./Components/premium/FrequentlyAskedQuestions";
-import GoPremium from "./Components/premium/GoPremium";
-import Footer from "./Components/premium/Footer";
+import PremiumRoute from "./Components/premium/PremiumRoute";
+
 
 
 function App() {
   return (
     <div className="App">
-      
-      <TopSection/>
-      <OptionsGrid/>
-      <YourGoalsSection/>
-      <VictoryStories/>
-      <FrequentlyAskedQuestions/>
-      <GoPremium/>
-      <Footer/>
+      <PremiumRoute/>
      
     </div>
   );
