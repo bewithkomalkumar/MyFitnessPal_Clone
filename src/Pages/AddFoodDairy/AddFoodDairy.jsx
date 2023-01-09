33a1c17a6731f57d57b26d3aa062cd6ee1eeebe2
komal kumar <er.kumarkomal@gmail.com>
2023-01-09 21:27:42 +0530

    for merging

--- src/Pages/AddFoodDairy/AddFoodDairy.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AddFoodDairy() {
-  return <div>AddFoodDairy</div>;
-}
-
-export default AddFoodDairy;
--- src/Redux/MyStore.jsx
+++
@@ -1,4 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import { FoodReducer } from "./FoodReducer";
-
-export const MyStore = createStore(FoodReducer);
